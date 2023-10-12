f96884b0961d5a0923df9da224009953868f47fa
ghxstloner <yoinerdeyhan@gmail.com>
2023-10-12 02:34:34 -0500

    creating header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from './app.component'; // Asegúrate de importar AppComponent
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, HeaderComponent], // Añade AppComponent a las declaraciones
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent], // Añade AppComponent al bootstrap
 })
-export class AppModule { }
+export class AppModule {}
