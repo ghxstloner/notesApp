2106f577f264db8caef46fb09b0e0801161a88a9
ghxstloner <yoinerdeyhan@gmail.com>
2023-10-21 01:31:24 -0500

    adding styles and logic to note list component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from './header/header.component';
+import { HeaderComponent } from '../header/header.component';
+import { NotesListComponent } from 'src/notes-list/notes-list.component';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  imports: [NotesListComponent, HeaderComponent],
 })
 export class AppComponent {
   title: any;
--- src/header/header.component.ts
+++ src/header/header.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
-  template: `<h1>{{ title }}</h1>`,
+  standalone: true,
+  template: '<h1>{{ title }}</h1>',
   styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent {
-  title = 'My notes';
+  title = 'My Notes';
 }
