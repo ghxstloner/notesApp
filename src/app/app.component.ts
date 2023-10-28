157d365f785488ec3a9c14f99605a295175f6a97
ghxstloner <yoinerdeyhan@gmail.com>
2023-10-28 00:42:02 -0500

    learning about forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { NotesListComponent } from 'src/notes-list/notes-list.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  imports: [NotesListComponent, HeaderComponent],
+  imports: [NotesListComponent, HeaderComponent, FormsModule],
 })
 export class AppComponent {
+  showName() {
+    alert(this.name);
+  }
+
   title: any;
+  name = 'Name';
 }
