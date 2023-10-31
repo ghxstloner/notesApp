c3f11fe0d0f22b26126a1383c2343608a88c953b
ghxstloner <yoinerdeyhan@gmail.com>
2023-10-30 21:24:57 -0500

    add the reactive form, to add a notes in the main component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { NotesListComponent } from 'src/notes-list/notes-list.component';
 import { FormsModule } from '@angular/forms';
+import { AddNoteComponent } from 'src/add-note/add-note.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  imports: [NotesListComponent, HeaderComponent, FormsModule],
+  imports: [NotesListComponent, HeaderComponent, FormsModule, AddNoteComponent],
 })
 export class AppComponent {
   showName() {
